f987e59f23b765b8513b84ab8b31cc39eaccdacb
maahd <maahdsh@gmail.com>
2019-02-12 01:34:41 +0100

    Centers everything. Cleans up the code.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -69,8 +69,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
 }
 
